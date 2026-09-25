96b61e5188886e63b8381f356c6d63d65b915db8
MiielsGH <athoil.kamil@gmail.com>
2026-09-25 13:31:07 +0700

    Creating Loading Screen using While Loop

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int TotalSteps = 100;
+int Step = 1;
+while (Step <= TotalSteps)
+{
+    Console.WriteLine($"Loading... Step {Step} of {TotalSteps}");
+    if (Step % 10 == 0)
+    {
+        Console.WriteLine($"loading {Step}% complete!");
+    }
+    Step++;
+}
